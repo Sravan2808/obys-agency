702da99845db8201069778967448d3d12ac28800
Sravan Surya <vsravansurya@gmail.com>
2025-01-11 16:26:33 +0530

    Page1Done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function loadingAnimation() {
+function loadingAnimation() { 
   var tl = gsap.timeline();
   tl.from(".line h1", {
     y: 150,
@@ -27,18 +27,37 @@ function loadingAnimation() {
   tl.to("#loader", {
     opacity: 0,
     duration: 0.2,
-    delay: 4,
+    delay: 0,
   });
   tl.from("#page1", {
     delay: 0.2,
     y: 1600,
     opacity: 0,
-    duration: 0.5,
+    duration: 0.6,
     ease: Power4,
   });
-  t1.to("#loader", {
+  tl.to("#loader", {
     display: "none",
   });
+  tl.from("#nav", {
+    opacity: 0,
+  });
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,h3,#hero4 h1 ", {
+    y: 120,
+    stagger: 0.2,
+  });
 }
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+
+  Shery.makeMagnet("#nav-part2 h4");
+}
+
 loadingAnimation();
- 
+cursorAnimation();
